940a422ae0918b9e5126ab5819726c6cb83e9eba
Pedro Adelino <pedroadmn@gmail.com>
2017-03-15 15:51:07 -0300

    Addin javadoc to LibraryClasse and Util

--- app/src/main/java/com/androidapp/getinline/util/LibraryClass.java
+++ app/src/main/java/com/androidapp/getinline/util/LibraryClass.java
@@ -12,12 +12,26 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class LibraryClass {
 
+    /**
+     * Static attribute PREF
+     */
     public static String PREF = "com.androidapp.getinline.PREF";
+
+    /**
+     * Firebase Database Reference
+     */
     private static DatabaseReference firebase;
 
+    /**
+     * Library Class contructor
+     */
     private LibraryClass() {
     }
 
+    /**
+     * Method to get Firebase Database Reference
+     * @return Firebase Database Reference
+     */
     public static DatabaseReference getFirebase() {
         if (firebase == null) {
             firebase = FirebaseDatabase.getInstance().getReference();
@@ -25,11 +39,23 @@ public class LibraryClass {
         return firebase;
     }
 
+    /**
+     * Method to save on shared preference the pair provider : token
+     * @param context Context
+     * @param key Key
+     * @param value Value
+     */
     public static void saveSP(Context context, String key, String value) {
         SharedPreferences sp = context.getSharedPreferences(PREF, Context.MODE_PRIVATE);
         sp.edit().putString(key, value).apply();
     }
 
+    /**
+     * Method to get token based on provider
+     * @param context Context
+     * @param key Key
+     * @return Token
+     */
     public static String getSP(Context context, String key) {
         SharedPreferences sp = context.getSharedPreferences(PREF, Context.MODE_PRIVATE);
         String token = sp.getString(key, "");
--- app/src/main/java/com/androidapp/getinline/util/Util.java
+++ app/src/main/java/com/androidapp/getinline/util/Util.java
@@ -9,18 +9,67 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Util {
+    /**
+     * Constant EMPTY
+     */
     public static final String EMPTY = "";
+
+    /**
+     * Constant EMAIL_PATTERN
+     */
     public static final String EMAIL_PATTERN = "^[a-zA-Z0-9#_~!$&'()*+,;=:.\"(),:;<>@\\[\\]\\\\]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*$";
+
+    /**
+     * Constant GOOGLE_USER_CONTENT_KEY
+     */
     public static final String GOOGLE_USER_CONTENT_KEY = "617151987057-es670jj5qfiuphb7u64gvrup3c8kdvum.apps.googleusercontent.com";
+
+    /**
+     * Constant FACEBOOK
+     */
     public static final String FACEBOOK = "facebook";
+
+    /**
+     * Constant GOOGLE
+     */
     public static final String GOOGLE = "google";
+
+    /**
+     * Constant NAME_KEY
+     */
     public static final String NAME_KEY = "name";
+
+    /**
+     * Constant EMAIL_KEY
+      */
     public static final String EMAIL_KEY = "email";
+
+    /**
+     * Constant USERS_KEY_DB
+     */
     public static final String USERS_KEY_DB = "users";
+
+    /**
+     * Constant PROFILE_PERMISSION
+     */
     public static final String PROFILE_PERMISSION = "public_profile";
+
+    /**
+     * Constant FRIENDS_PERMISSION
+     */
     public static final String FRIENDS_PERMISSION = "user_friends";
+
+    /**
+     * Constant EMAIL_PERMISSION
+     */
     public static final String EMAIL_PERMISSION = "email";
 
+    /**
+     * Method to validate user email address
+     * @param context Context
+     * @param email Email
+     * @return True if the email has a validate format, False otherwise
+     */
     public static boolean validateEmail(Context context, String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         Matcher matcher;
@@ -33,6 +82,13 @@ public class Util {
         return result;
     }
 
+    /**
+     * Method to validate if the passwords match
+     * @param context Context
+     * @param pass1Str Password
+     * @param pass2Str Password
+     * @return True if the passwords match, False otherwise
+     */
     public static boolean validateEqualPassword(Context context, String pass1Str, String pass2Str) {
         boolean result = true;
         if (!pass1Str.equals(pass2Str)) {
@@ -41,6 +97,4 @@ public class Util {
         }
         return result;
     }
-
-
 }
