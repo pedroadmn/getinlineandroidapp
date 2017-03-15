6583d090793a5d9403fd0602370789c2a47fb1f8
Pedro Adelino <pedroadmn@gmail.com>
2017-03-15 11:07:24 -0300

    Adding javadoc to CommomActivity

--- app/src/main/java/com/androidapp/getinline/activities/CommonActivity.java
+++ app/src/main/java/com/androidapp/getinline/activities/CommonActivity.java
@@ -9,31 +9,62 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 
-abstract public class CommonActivity extends AppCompatActivity {
+public abstract class CommonActivity extends AppCompatActivity {
 
+    /**
+     * User email variable
+     */
     protected AutoCompleteTextView email;
+
+    /**
+     * User password and confirmPassword variable
+     */
     protected EditText password, confirmPassword;
+
+    /**
+     * Visual indicator of progress in some operation
+     */
     protected ProgressBar progressBar;
 
+    /**
+     * Method to provide lightweight feedback about an operation
+     * @param message Message
+     */
     protected void showSnackBar(String message) {
         Snackbar.make(progressBar, message, Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
     }
 
+    /**
+     * Method to provides simple feedback about an operation in a small popup
+     * @param message Message
+     */
     protected void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
+    /**
+     * Method to make progress bar visible
+     */
     protected void openProgressBar() {
         progressBar.setVisibility(View.VISIBLE);
     }
 
+    /**
+     * Methdo to close progress bar
+     */
     protected void closeProgressBar() {
         progressBar.setVisibility(View.GONE);
     }
 
+    /**
+     * Abstract method where activities will init the views
+     */
     abstract protected void initViews();
 
+    /**
+     * Abstract method where activies will init an user
+     */
     abstract protected void initUser();
 
 }
