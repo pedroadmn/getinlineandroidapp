2c3a3d012c73cf301cfbcf02cc43ae3258d72419
Pedro Adelino <pedroadmn@gmail.com>
2017-03-15 10:36:31 -0300

    Making some organization readjustments in LoginActivity and Android Manifest. And Adding javadoc to LoginActivity

--- app/src/main/java/com/androidapp/getinline/activities/LoginActivity.java
+++ app/src/main/java/com/androidapp/getinline/activities/LoginActivity.java
@@ -38,8 +38,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.crash.FirebaseCrash;
 
-
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -53,22 +51,100 @@ import za.co.riggaroo.materialhelptutorial.tutorial.MaterialTutorialActivity;
 
 public class LoginActivity extends CommonActivity implements GoogleApiClient.OnConnectionFailedListener {
 
+    /**
+     * Google Request Code to onActivityResult
+     */
     private static final int RC_SIGN_IN_GOOGLE = 7859;
-    private static final int REQUEST_CODE = 1000;
+
+    /**
+     * Tutorial Request Code to onActivityResult
+     */
+    private static final int REQUEST_CODE_TUTORIAL = 1000;
+
+    /**
+     * The entry point of the Firebase Authentication SDK.
+     */
     private FirebaseAuth mAuth;
+
+    /**
+     * Listener called when there is a change in the authentication state.
+     */
     private FirebaseAuth.AuthStateListener mAuthListener;
+
+    /**
+     * The CallbackManager manages the callbacks into the FacebookSdk from an Activity's or
+     * Fragment's onActivityResult() method.
+     */
     private CallbackManager callbackManager;
+
+    /**
+     * The main entry point for Google Play services integration.
+     */
     private GoogleApiClient mGoogleApiClient;
+
+    /**
+     * Cancel Button to come back to main login page
+     */
     private TextView cancelButton;
+
+    /**
+     * A User object
+     */
     private User user;
-    String profileEmail;
-    Uri profilePhoto;
+
+    /**
+     * The profile email to be shown in the navigation list header
+     */
+    private String profileEmail;
+
+    /**
+     * The profile photo Uri to use in the navigation list header
+     */
+    private Uri profilePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        registerFacebookCallBack();
+        registerGoogleSignOption();
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = getFirebaseAuthResultHandler();
+
+        Log.d("ENTROUONCREATE", "ENTROUONCREATE");
+        initViews();
+        initUser();
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callCancel();
+            }
+        });
+    }
+
+    /**
+     * Method to build the google sign in configurations options
+     */
+    private void registerGoogleSignOption() {
+        // GOOGLE SIGN IN
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(Util.GOOGLE_USER_CONTENT_KEY)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    /**
+     * Method to register facebook callback to login and permissions
+     */
+    private void registerFacebookCallBack() {
         callbackManager = CallbackManager.Factory.create();
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -86,29 +162,6 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
                 showSnackBar(error.getMessage());
             }
         });
-
-        // GOOGLE SIGN IN
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(Util.GOOGLE_USER_CONTENT_KEY)
-                .requestEmail()
-                .build();
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = getFirebaseAuthResultHandler();
-        initViews();
-        initUser();
-
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                callCancel();
-            }
-        });
     }
 
     @Override
@@ -116,7 +169,6 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == RC_SIGN_IN_GOOGLE) {
-
             GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             GoogleSignInAccount account = googleSignInResult.getSignInAccount();
 
@@ -129,6 +181,7 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
             profilePhoto = account.getPhotoUrl();
 
             accessGoogleLoginData(account.getIdToken());
+
         } else {
             callbackManager.onActivityResult(requestCode, resultCode, data);
         }
@@ -148,14 +201,27 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
         }
     }
 
+    /**
+     * Intermediary Method to send facebook token credential to accessLoginData method
+     * @param accessToken Token given by Facebook
+     */
     private void accessFacebookLoginData(AccessToken accessToken) {
         accessLoginData(Util.FACEBOOK, (accessToken != null ? accessToken.getToken() : null));
     }
 
+    /**
+     * Intermediary Method to send google token credential to accessLoginData method
+     * @param accessToken Token given by Google
+     */
     private void accessGoogleLoginData(String accessToken) {
         accessLoginData(Util.GOOGLE, accessToken);
     }
 
+    /**
+     * Method to sign in on Firebase Authentication with the provider(Facebook or Google)
+     * @param provider Login provider name(Facebook or Google)
+     * @param tokens Token given by provider(Facebook or Google)
+     */
     private void accessLoginData(String provider, String... tokens) {
         if (tokens != null
                 && tokens.length > 0
@@ -170,8 +236,8 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (!task.isSuccessful()) {
-
                                 showSnackBar(getResources().getString(R.string.social_login_failed));
+                                closeProgressBar();
                             }
                         }
                     })
@@ -179,6 +245,7 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
                         @Override
                         public void onFailure(@NonNull Exception e) {
                             FirebaseCrash.report(e);
+                            closeProgressBar();
                         }
                     });
         } else {
@@ -186,6 +253,10 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
         }
     }
 
+    /**
+     * Method to take some action when the auth state change
+     * @return FirebaseAuth.AuthStateListener
+     */
     private FirebaseAuth.AuthStateListener getFirebaseAuthResultHandler() {
         FirebaseAuth.AuthStateListener callback = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -206,10 +277,19 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
         return callback;
     }
 
+    /**
+     *
+     * @param user User
+     * @param firebaseUser Firebase user
+     * @return True if user name is ok, and False otherwise
+     */
     private boolean isNameOk(User user, FirebaseUser firebaseUser) {
         return (user.getName() != null || firebaseUser.getDisplayName() != null);
     }
 
+    /**
+     * Method to initialize the views
+     */
     protected void initViews() {
         cancelButton = (TextView) findViewById(R.id.cancel_button);
         email = (AutoCompleteTextView) findViewById(R.id.email);
@@ -217,22 +297,37 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
         progressBar = (ProgressBar) findViewById(R.id.login_progress);
     }
 
+    /**
+     * Method to initialize the user
+     */
     protected void initUser() {
         user = new User();
         user.setEmail(email.getText().toString());
         user.setPassword(password.getText().toString());
     }
 
+    /**
+     * Method to switch from LoginActivity to SignUpActivity
+     * @param view View
+     */
     public void callSignUp(View view) {
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Method to switch from LoginActivity to ResetActiviy
+     * @param view View
+     */
     public void callReset(View view) {
         Intent intent = new Intent(this, ResetActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Method to make Login Form visible and main login screen invisible
+     * @param v View
+     */
     public void callLoginForm(View v) {
         findViewById(R.id.act_login_form).setVisibility(View.VISIBLE);
         findViewById(R.id.act_login_form).setEnabled(true);
@@ -240,6 +335,9 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
         findViewById(R.id.tv_learn_more).setVisibility(View.INVISIBLE);
     }
 
+    /**
+     * Method to make Login Form invisible and main login screen visible
+     */
     public void callCancel() {
         findViewById(R.id.act_login_form).setVisibility(View.INVISIBLE);
         findViewById(R.id.act_login_form).setEnabled(false);
@@ -247,14 +345,24 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
         findViewById(R.id.tv_learn_more).setVisibility(View.VISIBLE);
     }
 
+    /**
+     * Method to load the start tutorial
+     * @param view View
+     */
     public void loadTutorial(View view) {
-        Intent mainAct = new Intent(this, MaterialTutorialActivity.class);
-        mainAct.putParcelableArrayListExtra(MaterialTutorialActivity.MATERIAL_TUTORIAL_ARG_TUTORIAL_ITEMS, getTutorialItems(this));
-        startActivityForResult(mainAct, REQUEST_CODE);
-
+        Intent tutorialIntent = new Intent(this, MaterialTutorialActivity.class);
+        tutorialIntent.putParcelableArrayListExtra(MaterialTutorialActivity.MATERIAL_TUTORIAL_ARG_TUTORIAL_ITEMS, getTutorialItems(this));
+        startActivityForResult(tutorialIntent, REQUEST_CODE_TUTORIAL);
     }
 
+    /**
+     * Method to setup the start tutorial screens
+     * @param context Context
+     * @return Tutorial Item List
+     */
     private ArrayList<TutorialItem> getTutorialItems(Context context) {
+        ArrayList<TutorialItem> tutorialItems = new ArrayList<>();
+
         TutorialItem tutorialItem1 = new TutorialItem(context.getString(R.string.tutorial_first_message_title), context.getString(R.string.tutorial_first_message_subtitle),
                 R.color.colorPrimaryDark, R.drawable.social_network);
 
@@ -267,7 +375,6 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
         TutorialItem tutorialItem4 = new TutorialItem(context.getString(R.string.tutorial_fourth_message_title), context.getString(R.string.tutorial_fourth_message_subtitle),
                 R.color.seaGreen, R.drawable.social_network);
 
-        ArrayList<TutorialItem> tutorialItems = new ArrayList<>();
         tutorialItems.add(tutorialItem1);
         tutorialItems.add(tutorialItem2);
         tutorialItems.add(tutorialItem3);
@@ -276,24 +383,39 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
         return tutorialItems;
     }
 
+    /**
+     * Method to send login data. To conventional login (email and password)
+     * @param view View
+     */
     public void sendLoginData(View view) {
         FirebaseCrash.log("LoginActivity:clickListener:button:sendLoginData()");
         initUser();
         verifyLogin();
     }
 
+    /**
+     * Method to send facebook login data.
+     * @param view View
+     */
     public void sendLoginFacebookData(View view) {
         FirebaseCrash.log("LoginActivity:clickListener:button:sendLoginFacebookData()");
         LoginManager.getInstance().logInWithReadPermissions(this,
                 Arrays.asList(Util.PROFILE_PERMISSION, Util.FRIENDS_PERMISSION, Util.EMAIL_PERMISSION));
     }
 
+    /**
+     * Method to send google login data
+     * @param view
+     */
     public void sendLoginGoogleData(View view) {
         FirebaseCrash.log("LoginActivity:clickListener:button:sendLoginGoogleData()");
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN_GOOGLE);
     }
 
+    /**
+     * Method to switch from LoginActivity to MainActivity
+     */
     private void callMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         if(profileEmail != null && profilePhoto != null){
@@ -304,6 +426,9 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
         finish();
     }
 
+    /**
+     * Method to verify if the user is already logged.
+     */
     private void verifyLogged() {
         if (mAuth.getCurrentUser() != null) {
             callMainActivity();
@@ -312,18 +437,22 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
         }
     }
 
+    /**
+     * Method to verify the conventional login(email and password).
+     */
     private void verifyLogin() {
         FirebaseCrash.log("LoginActivity:verifyLogin()");
-        user.saveProviderSP(LoginActivity.this, Util.EMPTY);
         if (!isCredentialEmpty(user.getEmail(), user.getPassword())) {
             if (Util.validateEmail(getBaseContext(), user.getEmail())) {
                 openProgressBar();
+                user.saveProviderSP(LoginActivity.this, Util.EMPTY);
                 mAuth.signInWithEmailAndPassword(user.getEmail(), user.getPassword())
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (!task.isSuccessful()) {
                                     showSnackBar(getResources().getString(R.string.failed));
+                                    closeProgressBar();
                                     return;
                                 }
                             }
@@ -331,6 +460,7 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         FirebaseCrash.report(e);
+                        closeProgressBar();
                     }
                 });
             }
@@ -345,6 +475,12 @@ public class LoginActivity extends CommonActivity implements GoogleApiClient.OnC
         showSnackBar(connectionResult.getErrorMessage());
     }
 
+    /**
+     * Method to verify if the credentials are empty
+     * @param email Email
+     * @param password Password
+     * @return True if credentials are empty, and False otherwise
+     */
     public boolean isCredentialEmpty(String email, String password) {
         if (email.isEmpty() || password.isEmpty()) {
             showSnackBar(getResources().getString(R.string.empty_credentials));
